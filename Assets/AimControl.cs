1ab3ac6a91de12585407c25a415414c1ca464add
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2023-03-13 20:42:36 -0300

    feat: adds aim control and some assets for the ground and text

---
+++ Assets/AimControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimControl : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < spawnPoint.transform.position.z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, spawnPoint.transform.position.z);
+        }
+    }
+}
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Bocha
@@ -9,20 +11,28 @@ namespace Bocha
     {
         public float forceApplied;
 
+        [SerializeField] private GameObject guide;
         [SerializeField] private float multiplier;
+        [SerializeField] private Slider slider;
+        [SerializeField] private GameObject spawnPoint;
+
         private float min = 0f;
         private float max = 5f;
 
         private bool isPressed;
         private Rigidbody rb;
 
-        [SerializeField] private Slider slider;
 
         // Start is called before the first frame update
         void Start()
         {
             rb = GetComponent<Rigidbody>();
             //rb.maxAngularVelocity = 50f;
+        }
+
+        private void FixedUpdate()
+        {
+            MouseToWorld();
         }
 
         // Update is called once per frame
@@ -36,7 +46,7 @@ namespace Bocha
             {
                 isPressed = false;
                 //rb.AddTorque(forceApplied * multiplier, 0, 0, ForceMode.Impulse);
-                rb.AddForce(0, 0, forceApplied * multiplier, ForceMode.Impulse);
+                rb.AddRelativeForce(0, 0, forceApplied * multiplier, ForceMode.Impulse);
             }
 
             if (isPressed)
@@ -48,6 +58,22 @@ namespace Bocha
                 forceApplied = max;
 
             SetSlider();
+        }
+
+        private void MouseToWorld()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(ray, out var hitInfo, Mathf.Infinity);
+
+            var direction = hitInfo.point - ray.origin;
+
+            Debug.DrawRay(ray.origin, direction, Color.red);
+            Debug.DrawLine(spawnPoint.transform.position, hitInfo.point, Color.green);
+
+            guide.transform.position = hitInfo.point;
+
+            this.transform.LookAt(guide.transform.position);
         }
 
         public void SetSlider()
